e333eaf9565a31ea03ce3f866e72ea3f40e2a9f1
jbrekelmans <4394224+jbrekelmans@users.noreply.github.com>
2021-04-03 11:32:51 +1100

    fix incorrect handling of not found errors from storage

--- go/internal/pkg/service/gomodule/gocmd/util.go
+++ go/internal/pkg/service/gomodule/gocmd/util.go
@@ -7,7 +7,21 @@ import (
 	"strings"
 
 	"github.com/alessio/shellescape"
+
+	gomoduleservice "github.com/go-mod-proxy/go/internal/pkg/service/gomodule"
+	"github.com/go-mod-proxy/go/internal/pkg/service/storage"
 )
+
+func fdSeekToStart(fd *os.File) error {
+	offset, err := fd.Seek(0, io.SeekStart)
+	if err != nil {
+		return err
+	}
+	if offset != 0 {
+		return fmt.Errorf("(*os.File).Seek(0, io.SeekStart) returned unexpected non-zero offset %d", offset)
+	}
+	return nil
+}
 
 func formatArgs(args []string) string {
 	var sb strings.Builder
@@ -20,13 +34,11 @@ func formatArgs(args []string) string {
 	return sb.String()
 }
 
-func fdSeekToStart(fd *os.File) error {
-	offset, err := fd.Seek(0, io.SeekStart)
-	if err != nil {
+func mapStorageError(err error) error {
+	switch storage.GetErrorCode(err) {
+	case storage.NotFound:
+		return gomoduleservice.NewErrorf(gomoduleservice.NotFound, "%w", err)
+	default:
 		return err
 	}
-	if offset != 0 {
-		return fmt.Errorf("(*os.File).Seek(0, io.SeekStart) returned unexpected non-zero offset %d", offset)
-	}
-	return nil
 }
