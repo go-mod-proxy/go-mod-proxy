go-mod-proxy/go-mod-proxy

commit a6fa5f945910f2c88b3df0e9eafcfd890e4d9e31

add request logging and set "MaxHeaderBytes" to 5KiB (#54)

go/cmd/server/server.go
@@ -33,6 +33,9 @@ import (
 	servicestoragegcs "github.com/go-mod-proxy/go/internal/pkg/service/storage/gcs"
 )
 
+// Value of http.Server.MaxHeaderBytes
+const maxHeaderBytes = 5 * (1 << 10) // 5 Kibibytes (KiB)
+
 // CLI is a type reflected by "github.com/alecthomas/kong" that configures the CLI command for the client forward proxy.
 type CLI struct {
 	ConfigFile           string `required type:"existingfile" help:"Name of the YAML config file"`
@@ -261,7 +264,11 @@ func Run(ctx context.Context, opts *CLI) error {
 	if err != nil {
 		return err
 	}
-	err = http.ListenAndServe(fmt.Sprintf("0.0.0.0:%d", opts.Port), server)
+	err = (&http.Server{
+		Addr:           fmt.Sprintf("0.0.0.0:%d", opts.Port),
+		Handler:        server,
+		MaxHeaderBytes: maxHeaderBytes,
+	}).ListenAndServe()
 	if err != http.ErrServerClosed {
 		return err
 	}

go/internal/pkg/server/common/loggingmiddleware.go
@@ -1,0 +1,125 @@
+package common
+
+import (
+	"io"
+	"net/http"
+	"strings"
+	"time"
+
+	log "github.com/sirupsen/logrus"
+)
+
+type instrumentedReadCloser struct {
+	totalRead  int
+	readCloser io.ReadCloser
+}
+
+var _ io.ReadCloser = (*instrumentedReadCloser)(nil)
+
+func newInstrumentedReadCloser(readCloser io.ReadCloser) *instrumentedReadCloser {
+	return &instrumentedReadCloser{
+		readCloser: readCloser,
+	}
+}
+
+func (i *instrumentedReadCloser) Close() error {
+	return i.readCloser.Close()
+}
+
+func (i *instrumentedReadCloser) Read(p []byte) (n int, err error) {
+	n, err = i.readCloser.Read(p)
+	i.totalRead += n
+	return
+}
+
+type instrumentedResponseWriter struct {
+	writeHeaderCalled bool
+	statusCode        int
+	totalWritten      int
+	w                 http.ResponseWriter
+}
+
+var _ http.ResponseWriter = (*instrumentedResponseWriter)(nil)
+
+func newInstrumentedResponseWriter(w http.ResponseWriter) *instrumentedResponseWriter {
+	return &instrumentedResponseWriter{
+		w: w,
+	}
+}
+
+func (i *instrumentedResponseWriter) Header() http.Header {
+	return i.Header()
+}
+
+func (i *instrumentedResponseWriter) WriteHeader(statusCode int) {
+	i.writeHeaderCalled = true
+	i.statusCode = statusCode
+	i.w.WriteHeader(statusCode)
+}
+
+func (i *instrumentedResponseWriter) Write(p []byte) (n int, err error) {
+	n, err = i.w.Write(p)
+	i.totalWritten += n
+	return
+}
+
+func LoggingMiddleware(logger *log.Logger, lvl log.Level, prefix string) func(next http.Handler) http.Handler {
+	return func(next http.Handler) http.Handler {
+		return http.HandlerFunc(func(w http.ResponseWriter, req *http.Request) {
+			if !logger.IsLevelEnabled(lvl) {
+				next.ServeHTTP(w, req)
+				return
+			}
+			reqClone := req.Clone(req.Context())
+			instrumentedBody := newInstrumentedReadCloser(req.Body)
+			reqClone.Body = instrumentedBody
+			// NOTE: it is safe to assume req.GetBody is unused since we are dealing with server requests.
+
+			instrumentedResponseWriter := newInstrumentedResponseWriter(w)
+
+			start := time.Now()
+			next.ServeHTTP(instrumentedResponseWriter, reqClone)
+			elapsedTime := time.Since(start)
+
+			// If instrumentedResponseWriter.Write is called then instrumentedResponseWriter.WriteHeader will be called (by definition of http.ResponseWriter interface)
+			// but if instrumentedResponseWriter.Write and instrumentedResponseWriter.WriteHeader are both never called this equates to statusCode 200 OK
+			// with an empty body: https://github.com/golang/go/blob/acb189ea59d7f47e5db075e502dcce5eac6571dc/src/net/http/server.go#L1621
+
+			statusCode := instrumentedResponseWriter.statusCode
+			if !instrumentedResponseWriter.writeHeaderCalled {
+				statusCode = http.StatusOK
+			}
+
+			// instrumentedBody.totalRead = number of bytes in the request body after decoding transfer (i.e. "Transfer-Encoding: chunked")
+			//		but before any decompression (i.e. "Content-Encoding"). In case of errors from the underlying transport
+			//      (i.e. client closes connection) the number may be less than what the client sent.
+			//
+			// instrumentedResponseWriter.totalWritten = number of bytes in the response body before transfer encoding and after compression
+			//		(assuming this middleware is run before any compression middleware).
+
+			path := req.URL.EscapedPath()
+			var pathAndQuery strings.Builder
+			pathAndQuery.Grow(len(path) + len(req.URL.RawQuery) + 1)
+			pathAndQuery.WriteString(path)
+			if req.URL.ForceQuery || req.URL.RawQuery != "" {
+				pathAndQuery.WriteByte('?')
+			}
+			pathAndQuery.WriteString(req.URL.RawQuery)
+
+			// X-Forwarded-For is a common header respected by many load-balancers.
+			// The idea is that whenever a load-balancer forwards a request it appends the
+			// client IP address to the list in the X-Forwarded-For header. If all load-balancers between a client and server
+			// follow this behaviour then the first element in the list is the client IP address as seen by the first load-balancer.
+			var xForwardedFor []string
+			for _, x := range req.Header.Values("X-Forwarded-For") {
+				for _, xElem := range strings.Split(",", x) {
+					xElemTrimmed := strings.Trim(xElem, " ")
+					xForwardedFor = append(xForwardedFor, xElemTrimmed)
+				}
+			}
+			log.NewEntry(logger).Logf(lvl, "%s: %s %#v status=%d duration=%dms requestBodySize=%d responseBodySize=%d xForwardedFor=%#v remoteAddr=%#v",
+				prefix, req.Method, pathAndQuery.String(), statusCode, elapsedTime.Milliseconds(), instrumentedBody.totalRead, instrumentedResponseWriter.totalWritten,
+				strings.Join(xForwardedFor, ", "), req.RemoteAddr)
+		})
+	}
+}

go/internal/pkg/server/server.go
@@ -67,13 +67,13 @@ func NewServer(opts ServerOptions) (*Server, error) {
 	if opts.Transport == nil {
 		return nil, fmt.Errorf("opts.Transport must not be nil")
 	}
-	router := mux.NewRouter().UseEncodedPath().SkipClean(true)
 	s := &Server{
 		accessTokenAuthenticator: opts.AccessTokenAuthenticator,
 		identityStore:            opts.IdentityStore,
 		realm:                    opts.Realm,
-		router:                   router,
 	}
+	s.router = mux.NewRouter().UseEncodedPath().SkipClean(true)
+	s.router.Use(servercommon.LoggingMiddleware(log.StandardLogger(), log.InfoLevel, "request"))
 	var accessTokenAuthenticatorFunc func(w http.ResponseWriter, req *http.Request) *serviceauth.Identity
 	authRouter := s.router.PathPrefix("/auth/").Subrouter()
 	if opts.ClientAuthEnabled {
@@ -105,7 +105,7 @@ func NewServer(opts ServerOptions) (*Server, error) {
 		}
 	}
 	_, err := servergosumdbproxy.NewServer(servergosumdbproxy.ServerOptions{
-		ParentRouter:     router,
+		ParentRouter:     s.router,
 		SumDatabaseProxy: opts.SumDatabaseProxy,
 		Transport:        opts.Transport,
 	})

commit 1a91418f1d4480bbbdd68c157ef4ca8140ee7085

Add golang linting and fix stackoverflow bug introduced in #54 (#55)
* fix stackoverflow bug
* add golang linting and fix linting issues

.github/workflows/ci.yaml
@@ -19,6 +19,17 @@ jobs:
       - name: Build
         run: ./scripts/main.sh build
 
+      - name: "🧐 Lint"
+        uses: golangci/golangci-lint-action@v2
+        with:
+          # Possible flags to set in args are documented here: https://golangci-lint.run/usage/configuration/
+          args: --timeout 10m
+          skip-build-cache: true
+          skip-go-installation: true
+          skip-pkg-cache: true
+          version: v1.29
+          working-directory: go
+
       - name: Docker Login
         uses: docker/login-action@v1
         with:
@@ -31,7 +42,7 @@ jobs:
           IMAGE=jbrekelmans/go-module-proxy:$GITHUB_SHA_SHORT
           docker tag jbrekelmans/go-module-proxy:latest "${IMAGE}"
           docker push "${IMAGE}"
-      
+
       - name: Docker Push (latest)
         if: ${{ github.ref == 'ref/head/master' }}
         run: |

go/cmd/clientforwardproxy/clientforwardproxy.go
@@ -13,6 +13,7 @@ import (
 )
 
 // CLI is a type reflected by "github.com/alecthomas/kong" that configures the CLI command for the client forward proxy.
+//nolint:structtag // linter does not like the syntax required by the kong package
 type CLI struct {
 	Password  string `required help:"Password component of credentials to access server"`
 	Port      int    `required help:"Port to listen on"`

go/cmd/credentialhelper/credentialhelper.go
@@ -6,6 +6,7 @@ import (
 )
 
 // CLI is a type reflected by "github.com/alecthomas/kong" that configures the CLI command for the client forward proxy.
+//nolint:structtag // linter does not like the syntax required by the kong package
 type CLI struct {
 	GoModulePath string   `required help:"Go module path"`
 	Port         int      `required help:"Port on 127.0.0.1 that the server is listening on"`

go/cmd/main.go
@@ -25,6 +25,7 @@ func main() {
 }
 
 // CLI is a definition for kong command line parser
+//nolint:structtag // linter does not like the syntax required by the kong package
 var CLI struct {
 	LogLevel string `help:"Log level. Must be case-insensitive equal to one of trace, debug, info, warning, error, panic and fatal"`
 
@@ -56,5 +57,4 @@ func mainCore() error {
 	default:
 		panic(kongCtx.Command())
 	}
-	return nil
 }

go/cmd/server/server.go
@@ -37,6 +37,7 @@ import (
 const maxHeaderBytes = 5 * (1 << 10) // 5 Kibibytes (KiB)
 
 // CLI is a type reflected by "github.com/alecthomas/kong" that configures the CLI command for the client forward proxy.
+//nolint:structtag // linter does not like the syntax required by the kong package
 type CLI struct {
 	ConfigFile           string `required type:"existingfile" help:"Name of the YAML config file"`
 	Port                 int    `required help:"Port to listen on"`
@@ -117,12 +118,18 @@ func Run(ctx context.Context, opts *CLI) error {
 		if err != nil {
 			return err
 		}
-		googleHTTPClient = oauth2.NewClient(nil, googleCredentials.TokenSource)
-		// A slightly hacky way to reuse httpClient's Transport instead of having another one.
-		googleHTTPClient.Transport.(*oauth2.Transport).Base = httpClient.Transport
-		googleHTTPClientBypassProxy = oauth2.NewClient(nil, googleCredentials.TokenSource)
-		// A slightly hacky way to reuse httpClient's Transport instead of having another one.
-		googleHTTPClientBypassProxy.Transport.(*oauth2.Transport).Base = httpClientBypassProxy.Transport
+		googleHTTPClient = &http.Client{
+			Transport: &oauth2.Transport{
+				Base:   httpClient.Transport,
+				Source: googleCredentials.TokenSource,
+			},
+		}
+		googleHTTPClientBypassProxy = &http.Client{
+			Transport: &oauth2.Transport{
+				Base:   httpClientBypassProxy.Transport,
+				Source: googleCredentials.TokenSource,
+			},
+		}
 	}
 	var storage servicestorage.Storage
 	if cfg.Storage.GCS != nil {
@@ -167,6 +174,9 @@ func Run(ctx context.Context, opts *CLI) error {
 			cfg.ClientAuth.Authenticators.AccessToken.TimeToLive,
 			identityStore,
 		)
+		if err != nil {
+			return err
+		}
 		if !enableGCEAuth {
 			log.Infof("not enabling GCE authentication because .clientAuth.enabled is not true or no element of .clientAuth.identities in %#v has .gceInstanceIdentityBinding != null",
 				opts.ConfigFile)

go/internal/pkg/server/common/loggingmiddleware.go
@@ -48,7 +48,7 @@ func newInstrumentedResponseWriter(w http.ResponseWriter) *instrumentedResponseW
 }
 
 func (i *instrumentedResponseWriter) Header() http.Header {
-	return i.Header()
+	return i.w.Header()
 }
 
 func (i *instrumentedResponseWriter) WriteHeader(statusCode int) {

go/internal/pkg/server/credentialhelper/git/git.go
@@ -85,7 +85,7 @@ func (s *Server) post(w http.ResponseWriter, req *http.Request) {
 		}
 		w.Header().Set("Content-Type", "application/json")
 		w.WriteHeader(http.StatusOK)
-		json.NewEncoder(w).Encode(UserPassword{
+		_ = json.NewEncoder(w).Encode(UserPassword{
 			User:     "x-access-token",
 			Password: token,
 		})
@@ -94,7 +94,6 @@ func (s *Server) post(w http.ResponseWriter, req *http.Request) {
 	error := fmt.Sprintf("TODO implement a mechanism to retrieve credentials for non-GitHub repositories (%#v)", goModulePath)
 	log.Debugf("git credential helper server: %s", error)
 	http.Error(w, error, http.StatusInternalServerError)
-	return
 }
 
 type UserPassword struct {

go/internal/pkg/service/gomodule/gocmd/cmd.go
@@ -23,6 +23,7 @@ func (s *Service) runCmd(ctx context.Context, t *tempGoEnv, args []string) (stdo
 		return
 	}
 	err = cmd.Wait()
+	cmdStdoutStderr.done()
 	stdout = cmdStdoutStderr.stdout()
 	strLog = cmdStdoutStderr.getLog()
 	if err != nil {
@@ -82,16 +83,12 @@ func (c *cmdStdoutStderr) done() {
 }
 
 func (c *cmdStdoutStderr) getLog() string {
-	return string(c.log.Bytes())
+	return c.log.String()
 }
 
 func (c *cmdStdoutStderr) register(cmd *exec.Cmd) {
 	cmd.Stdout = newCmdStdoutStderrWriter(c, stdout)
 	cmd.Stderr = newCmdStdoutStderrWriter(c, stderr)
-}
-
-func (c *cmdStdoutStderr) stderr() []byte {
-	return c.perFDStates[int(stderr)].data
 }
 
 func (c *cmdStdoutStderr) stdout() []byte {
@@ -118,7 +115,6 @@ func (c *cmdStdoutStderr) write(fd wellKnownFileDescriptor, p []byte) {
 		perFDState.scanStartPos = i + 1
 		fmt.Fprintf(&c.log, "%s: %s\n", fd.String(), string(line))
 	}
-	return
 }
 
 type cmdStdoutStderrWriter struct {

go/internal/pkg/service/gomodule/gocmd/maxparallelismresourcepool.go
@@ -37,10 +37,6 @@ func (m *maxParallelismResourcePool) acquire(ctx context.Context) (maxParallelis
 	return r, nil
 }
 
-func (m *maxParallelismResourcePool) len() int {
-	return cap(m.c)
-}
-
 func (m *maxParallelismResourcePool) newMaxParallelismResource() maxParallelismResource {
 	released := false
 	r := func() {
@@ -49,16 +45,6 @@ func (m *maxParallelismResourcePool) newMaxParallelismResource() maxParallelismR
 		}
 		released = true
 		m.c <- struct{}{}
-	}
-	return r
-}
-
-func (m *maxParallelismResourcePool) tryAcquire() maxParallelismResource {
-	r := m.newMaxParallelismResource()
-	select {
-	case <-m.c:
-	default:
-		return nil
 	}
 	return r
 }

go/internal/pkg/service/gomodule/gocmd/service.go
@@ -886,9 +886,7 @@ func (s *Service) notFoundOptimizations(modulePath string) (err error) {
 				return
 			}
 			// One slash
-		} else {
-			// No slashes
-		}
+		} // No slashes
 		err = gomoduleservice.NewErrorf(gomoduleservice.NotFound, `invalid github.com import path %#v`, modulePath)
 		return
 	}