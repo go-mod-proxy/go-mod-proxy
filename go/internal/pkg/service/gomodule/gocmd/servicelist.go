09aa315ff9e5e1a23c28982248705be7c7f0c76f
jbrekelmans <4394224+jbrekelmans@users.noreply.github.com>
2021-10-30 12:55:45 +1100

    move list to new file

---
+++ go/internal/pkg/service/gomodule/gocmd/servicelist.go
@@ -0,0 +1,253 @@
+package gocmd
+
+import (
+	"bytes"
+	"context"
+	"fmt"
+	"io"
+	"io/ioutil"
+	"strings"
+	"sync"
+
+	log "github.com/sirupsen/logrus"
+	module "golang.org/x/mod/module"
+
+	"github.com/go-mod-proxy/go-mod-proxy/go/internal/pkg/modproxyclient"
+	gomoduleservice "github.com/go-mod-proxy/go-mod-proxy/go/internal/pkg/service/gomodule"
+	"github.com/go-mod-proxy/go-mod-proxy/go/internal/pkg/service/storage"
+	"github.com/go-mod-proxy/go-mod-proxy/go/internal/pkg/util"
+)
+
+func (s *Service) List(ctx context.Context, modulePath string) (io.ReadCloser, error) {
+	if err := s.notFoundOptimizations(modulePath); err != nil {
+		return nil, err
+	}
+	privateModulesElement := s.getPrivateModulesElement(modulePath)
+	if privateModulesElement == nil && !s.readAfterListIsStronglyConsistent {
+		goListVersions, err := s.listViaParentProxy(ctx, modulePath)
+		if err != nil {
+			return nil, err
+		}
+		var sb strings.Builder
+		for _, version := range goListVersions {
+			sb.WriteString(version)
+			sb.WriteByte('\n')
+		}
+		return ioutil.NopCloser(strings.NewReader(sb.String())), nil
+	}
+	versionMap := map[string]struct{}{}
+	versionMapMutex := new(sync.Mutex)
+	errChan := make(chan error)
+	ctx, cancelFunc := context.WithCancel(ctx)
+	defer cancelFunc()
+	go s.listAddObjectNames(ctx, errChan, storageGoModObjNamePrefix+modulePath+"@", versionMap, versionMapMutex)
+	go s.listAddObjectNames(ctx, errChan, storageConcatObjNamePrefix+modulePath+"@", versionMap, versionMapMutex)
+	var goListVersions []string
+	go func() {
+		var err error
+		if privateModulesElement == nil {
+			goListVersions, err = s.listViaParentProxy(ctx, modulePath)
+		} else {
+			goListVersions, err = s.listGoCmd(ctx, modulePath)
+		}
+		errChan <- err
+	}()
+	var err error
+	// The constant 3 must be aligned with the number of Goroutines above, and each Goroutine must send on errChan exactly once.
+	for i := 0; i < 3; i++ {
+		err2 := <-errChan
+		if err2 != nil {
+			if err == nil {
+				err = err2
+				cancelFunc()
+			} else {
+				select {
+				case <-ctx.Done():
+					if err == ctx.Err() {
+						continue
+					}
+				default:
+				}
+				log.Errorf("got secondary error while listing versions: %v", err)
+			}
+		}
+	}
+	if err != nil {
+		return nil, err
+	}
+	if !s.readAfterListIsStronglyConsistent {
+		for _, version := range goListVersions {
+			versionMap[version] = struct{}{}
+		}
+	} else {
+		var waitGroup sync.WaitGroup
+		for _, version := range goListVersions {
+			versionMapMutex.Lock()
+			_, flag := versionMap[version]
+			versionMapMutex.Unlock()
+			if flag {
+				continue
+			}
+			version := version
+			runCmdResource, err := s.runCmdResourcePool.acquire(ctx)
+			if err != nil {
+				return nil, err
+			}
+			waitGroup.Add(1)
+			go func() {
+				defer runCmdResource.release()
+				defer waitGroup.Done()
+				tempGoEnv, err := s.newTempGoEnv()
+				if err != nil {
+					log.Error(err)
+					return
+				}
+				defer func() {
+					err := tempGoEnv.removeRef()
+					if err != nil {
+						log.Errorf("error removing tmpDir %#v of *tempGoEnv: %v", tempGoEnv.TmpDir, err)
+					}
+				}()
+				_, _, _, err = s.getGoModuleAndIndexIfNeeded(ctx, tempGoEnv, &module.Version{Path: modulePath, Version: version}, runCmdResource)
+				if err != nil {
+					log.Error(err)
+					return
+				}
+				versionMapMutex.Lock()
+				versionMap[version] = struct{}{}
+				versionMapMutex.Unlock()
+			}()
+		}
+		waitGroup.Wait()
+	}
+	var sb strings.Builder
+	for version := range versionMap {
+		sb.WriteString(version)
+		sb.WriteByte('\n')
+	}
+	return ioutil.NopCloser(strings.NewReader(sb.String())), nil
+}
+
+func (s *Service) listAddObjectNames(ctx context.Context, errChan chan<- error, namePrefix string,
+	versionMap map[string]struct{}, versionMapMutex *sync.Mutex) {
+	var pageToken string
+	for {
+		var objList *storage.ObjectList
+		objList, err := s.storage.ListObjects(ctx, storage.ObjectListOptions{
+			NamePrefix: namePrefix,
+			PageToken:  pageToken,
+		})
+		if err != nil {
+			errChan <- mapStorageError(err)
+			return
+		}
+		versionMapMutex.Lock()
+		for _, name := range objList.Names {
+			version := name[len(namePrefix):]
+			if !module.IsPseudoVersion(version) {
+				versionMap[version] = struct{}{}
+			}
+		}
+		versionMapMutex.Unlock()
+		pageToken = objList.NextPageToken
+		if pageToken == "" {
+			break
+		}
+	}
+	errChan <- nil
+}
+
+func (s *Service) listGoCmd(ctx context.Context, modulePath string) (goListVersions []string, err error) {
+	tempGoEnv, err := s.newTempGoEnv()
+	if err != nil {
+		return
+	}
+	defer func() {
+		err2 := tempGoEnv.removeRef()
+		if err2 != nil {
+			if err == nil {
+				err = err2
+			} else {
+				log.Errorf("error removing tmpDir %#v of *tempGoEnv: %v", tempGoEnv.TmpDir, err2)
+			}
+		}
+	}()
+	runCmdResource, err := s.runCmdResourcePool.acquire(ctx)
+	if err != nil {
+		return
+	}
+	defer runCmdResource.release()
+	err = s.initializeTempGoEnvForModule(ctx, tempGoEnv, modulePath)
+	if err != nil {
+		return
+	}
+	args := []string{s.goBinFile, "list", "-m", "-versions", "-json", modulePath}
+	stdout, strLog, err := s.runCmd(ctx, tempGoEnv, args)
+	if err != nil {
+		return
+	}
+	runCmdResource.release()
+	var goListInfo goModuleInfo
+	err = util.UnmarshalJSON(bytes.NewReader(stdout), &goListInfo, true)
+	if err != nil {
+		err = fmt.Errorf("command %s succeeded but got unexpected stderr/stdout:\n%s", formatArgs(args), strLog)
+		return
+	}
+	goListVersions = goListInfo.Versions
+	if len(goListInfo.Versions) == 0 {
+		// NOTE: if len(goListInfo.Versions) == 0 then we will have actually queried the latest version of the module (if the command exited
+		// with code 0). Use this information and index the version.
+		go s.listGoCmdIndex(&module.Version{
+			Path:    modulePath,
+			Version: goListInfo.Version,
+		})
+	}
+	return
+}
+
+func (s *Service) listGoCmdIndex(moduleVersion *module.Version) {
+	log.Tracef(`discovered latest version (%#v) of module %#v through a \"go list\" command, eagerly caching this module version...`,
+		moduleVersion.Version, moduleVersion.Path)
+	ctx := context.Background()
+	_, err := s.infoFromGoModObj(ctx, moduleVersion)
+	if err == nil {
+		log.Tracef(`latest version (%#v) of module %#v discovered through a \"go list\" command is already cached`,
+			moduleVersion.Version, moduleVersion.Path)
+		return
+	}
+	if !gomoduleservice.ErrorIsCode(err, gomoduleservice.NotFound) {
+		log.Errorf(`error checking if latest version (%#v) of module %#v discovered through a \"go list\" command is already cached: %v`,
+			moduleVersion.Version, moduleVersion.Path, err)
+	}
+	runCmdResource, err := s.runCmdResourcePool.acquire(ctx)
+	if err != nil {
+		panic(err)
+	}
+	defer runCmdResource.release()
+	tempGoEnv, err := s.newTempGoEnv()
+	if err != nil {
+		log.Error(err)
+		return
+	}
+	defer func() {
+		err := tempGoEnv.removeRef()
+		if err != nil {
+			log.Errorf("error removing tmpDir %#v of *tempGoEnv: %v", tempGoEnv.TmpDir, err)
+		}
+	}()
+	_, _, _, err = s.getGoModuleAndIndexIfNeeded(ctx, tempGoEnv, moduleVersion, runCmdResource)
+	if err != nil {
+		log.Errorf(`error caching latest version (%#v) of module %#v discovered through a \"go list\" command: %v`,
+			moduleVersion.Version, moduleVersion.Path, err)
+	}
+}
+
+func (s *Service) listViaParentProxy(ctx context.Context, modulePath string) (goListVersions []string, err error) {
+	// For public modules send a request to the parent proxy directly instead of via a "go list" command.
+	// This also avoids an unnecessary second request performed by a "go list" command when GET "/@v/list" returns zero versions.
+	goListVersions, err = modproxyclient.List(ctx, s.parentProxyURL, s.httpClient, modulePath)
+	if err == modproxyclient.ErrNotFound {
+		err = gomoduleservice.NewErrorf(gomoduleservice.NotFound, "%v", err)
+	}
+	return
+}
